7349de75f3a6b7cd1ce487b39ac9e8f2910d78d0
Yiyang Kok <yiyangkok@gmail.com>
2017-04-03 14:32:04 -0700

    search history function done

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ var validURL = require('valid-url');
 var mongowrap = require('./scripts/mongowrap.js');
 var urlCounter = 0;
 
-// Use connect method to connect to the Server
-// Initial query to mongodb to check the # of existing urls.
-mongowrap.getSize(function(size) {
-  urlCounter = size;
-  console.log("URL COUNTER IS " + urlCounter);
-});
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
@@ -22,24 +15,20 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.get('/new/*', function(request, response) {
-  // Process the url into mongodb.
-  if (validURL.isUri(request.params['0'])) {
-    mongowrap.addEntry(urlCounter, request.params['0'], function(shortID) {
-      urlCounter++;
-      var temp = {'original_url':request.params['0'], 'short_url': 'https://url-shortener-decky.herokuapp.com/' + shortID};
-      response.send(temp);
-    });
-  } else {
-    // Not valid URL, send Error
-    response.send({"ERROR": "Invalid URL"});
-  }
+app.get('/api/imagesearch/:SEARCHTERM', function(request, response) {
+  console.log(request.params.SEARCHTERM);
+  console.log(request.query);
+  // Process the searchterm into mongodb
+  mongowrap.addTerm(request.params.SEARCHTERM);
+  // Process the search request
+  // SOME CALL TO GOOGLE API GOES HERE.
+
 });
 
-app.get('/:INTEGER', function(request, response) {
+app.get('/api/latest/imagesearch/', function(request, response) {
   // Retrieve the original url from the db and send the page to the user.
-  mongowrap.retrieveEntry(parseInt(request.params.INTEGER), function(fullURL) {
-    response.redirect(fullURL);
+  mongowrap.getHistory(function(history) {
+    response.send(history);
   });
 });
 
